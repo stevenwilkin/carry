fc7d1b72985f9abb3f68b4dcfb6d21d6b18ab0e3
Steven Wilkin <stevenwilkin@gmail.com>
2022-04-29 22:31:29 +0700

    Ignore positions with zero size

--- deribit/positions.go
+++ deribit/positions.go
@@ -16,21 +16,25 @@ func (d *Deribit) GetPositions() []Position {
 		return []Position{}
 	}
 
-	result := make([]Position, len(response.Result))
-	timestamps := make([]int, len(response.Result))
+	timestamps := []int{}
 	unsorted := map[int]Position{}
 
-	for i, position := range response.Result {
+	for _, position := range response.Result {
+		if position.Size == 0 {
+			continue
+		}
+
 		t, err := time.Parse("2Jan06", position.InstrumentName[4:])
 		if err != nil {
 			continue
 		}
 
 		timestamp := int(t.Unix())
-		timestamps[i] = timestamp
+		timestamps = append(timestamps, timestamp)
 		unsorted[timestamp] = position
 	}
 
+	result := make([]Position, len(timestamps))
 	sort.Ints(timestamps)
 
 	for i, timestamp := range timestamps {
